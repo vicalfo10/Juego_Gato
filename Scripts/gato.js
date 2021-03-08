74ff4ac90ed8b697d660bfb18666733aa92152ca
vicalfo10 <vgranados_cr_90@hotmail.com>
2021-03-08 12:52:46 -0600

    Agrear jugadores

--- Scripts/gato.js
+++ Scripts/gato.js
@@ -3,6 +3,11 @@ var simboloGanador = ""
 var Ganados = [0, 0, 0]
 var turno = 1
 var resultado = false
+var jugador1 = document.getElementById("jugador1")
+var jugador2 = document.getElementById("jugador2")
+var botonX = document.getElementById("contadorX")
+var botonO = document.getElementById("contadorO")
+var botonE = document.getElementById("empate")
 var a1 = document.getElementById("a1")
 var a2 = document.getElementById("a2")
 var a3 = document.getElementById("a3")
@@ -12,14 +17,27 @@ var b3 = document.getElementById("b3")
 var c1 = document.getElementById("c1")
 var c2 = document.getElementById("c2")
 var c3 = document.getElementById("c3")
-var botonX = document.getElementById("contadorX")
-var botonO = document.getElementById("contadorO")
-var botonE = document.getElementById("empate")
+
+var reiniciar = document.getElementById("reiniciar")
+reiniciar.addEventListener("click", () => {
+    reiniciarBtn()
+})
 
 var limpiar = document.getElementById("limpiar")
 limpiar.addEventListener("click", () => {
     limpiarBtn()
 })
+
+function reiniciarBtn(){
+    jugador1.disabled = false
+    jugador2.disabled = false
+    jugador1.value = ""
+    jugador2.value = ""
+    botonX.innerHTML = "X: 0"
+    botonE.innerHTML = "E: 0"
+    botonO.innerHTML = "O: 0"
+    limpiarBtn()
+}
 
 function limpiarBtn(){
     a1.innerHTML = ""
@@ -47,14 +65,24 @@ function obtenerId(id){
     let simbolo = ["X", "O"]
     let asignar = document.getElementById(id)
 
-    if(turno == 0){
-        asignar.innerHTML = simbolo[1]
-        turno = 1
-    }else{
-        asignar.innerHTML = simbolo[0]
-        turno = 0
+    if(asignar.innerHTML == ""){
+        if(jugador1.value == ""){
+            swal("Se requiere un jugador válido", "Jugador #1 no puede estar vacío", "warning")
+        }else if(jugador2.value == ""){
+            swal("Se requiere un jugador válido", "Jugador #2 no puede estar vacío", "warning")
+        }else{
+            jugador1.disabled = true
+            jugador2.disabled = true
+            if(turno == 0){
+                asignar.innerHTML = simbolo[1]
+                turno = 1
+            }else{
+                asignar.innerHTML = simbolo[0]
+                turno = 0
+            }
+            validaGanador(asignar.innerHTML) 
+        }
     }
-    validaGanador(asignar.innerHTML)
 }
 
 function validaGanador(valor){
@@ -102,10 +130,13 @@ function validaGanador(valor){
 
 function muestraGanador(resultado, simboloGanador){
     if(resultado){
-        if(simboloGanador == "X" || simboloGanador == "O"){
-            swal("Buen Trabajo!", "Felicidades, Ganador: " + simboloGanador, "success");
-            limpiarBtn()
+        if(simboloGanador == "X"){
+            swal("Buen Trabajo!", "Felicidades, Ganador es: " + jugador1.value, "success");
+            
+        }else{
+            swal("Buen Trabajo!", "Felicidades, Ganador es: " + jugador2.value, "success");
         }
+        limpiarBtn()
     }else if(simboloGanador == "E"){
         swal("No hay gandor!", "Empate en el resultado", "info");
         limpiarBtn()
